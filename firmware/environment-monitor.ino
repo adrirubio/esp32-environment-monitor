adbcd35cedbbf50e4727aa82e8aebed4790ebba3
Adrian Rubio-Punal <adrian.rubio.punal@gmail.com>
2025-08-21 15:58:20 +0200

    Add setup, pressure and humidity

--- firmware/environment-monitor.ino
+++ firmware/environment-monitor.ino
@@ -12,20 +12,43 @@ uint32_t getAbsoluteHumidity(float temperature, float humidity) {
   return static_cast<uint32_t>(absHumidity * 1000.0f); // mg/m³
 }
 
+void setup() {
+  Serial.begin(115200);
+  while (!Serial) { delay(10); }
+
+  if (!bme.begin(0x76)) {
+    Serial.println("BME280 not found!");
+    while (1) delay(10);
+  }
+
+  if (!sgp.begin()) {
+    Serial.println("SGP30 not found!");
+    while (1) delay(10);
+  }
+}
+
 void loop() {
+  // BME280 measurements
   float temperature = bme.readTemperature(); // °C
-  float humidity = bme.readHumidity();       // %
+  float humidity = bme.readHumidity(); // %
+  float pressure = bme.readPressure(); // hPa
+  float altitude = bme.readAltitude(SEALEVELPRESSURE_HPA); // m
 
   // feed absolute humidity to SGP30
   sgp.setHumidity(getAbsoluteHumidity(temperature, humidity));
 
+  // SGP30 measurements
   if (!sgp.IAQmeasure()) {
     Serial.println("SGP30 measurement failed");
     return;
   }
 
+  // print to serial monitor
   Serial.print("Temperature: "); Serial.print(temperature); Serial.print(" °C, ");
   Serial.print("Humidity: "); Serial.print(humidity); Serial.println(" %");
+
+  Serial.print("Pressure: "); Serial.print(pressure); Serial.print(" hPa, ");
+  Serial.print("Altitude: "); Serial.print(altitude); Serial.print(" m")
 
   Serial.print("eCO2: "); Serial.print(sgp.eCO2); Serial.print(" ppm, ");
   Serial.print("TVOC: "); Serial.println(sgp.TVOC);
